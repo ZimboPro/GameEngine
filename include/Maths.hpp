4d9112ffb067912d17528ed13a6f063ca192b809
ZimboPro <dejagerleslie@gmail.com>
2018-08-01 20:59:28 +0200

    using glm

--- include/Maths.hpp
+++ include/Maths.hpp
@@ -1,15 +1,10 @@
 #ifndef MATHS_HPP
 #define MATHS_HPP
 
+#include <glm/glm.hpp>
+
 #include <maths/Vec2.hpp>
 #include <maths/Vec3.hpp>
 #include <maths/Vec4.hpp>
 
-namespace GameEngine
-{
-    namespace maths
-    {
-
-    }
-}
 #endif
